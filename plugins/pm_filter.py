274a05d2a3a58e276547cc25b06bb2be6900545f
mhlinkss <107099913+mhlinkss@users.noreply.github.com>
2022-06-08 13:18:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -566,8 +566,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about_menu":
         buttons = [[
-        InlineKeyboardButton('ᴅᴋ ʙᴏᴛx', url='https://t.me/dk_botx'),
-        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/dkbotxchats'),
+        InlineKeyboardButton('Movie hub', url='https://t.me/moviehubofficial18'),
+        InlineKeyboardButton('Req Movie here ', url='https://t.me/moviereqgroup_movieshub'),
         InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
